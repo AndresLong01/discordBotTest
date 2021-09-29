792fcedf07f9c32558710eb996cdf80d68568e55
Andres Long <andreslong95@gmail.com>
2021-09-29 00:52:54 -0500

    version 1.0 audio player

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const Discord = require("discord.js")
-require('dotenv').config()
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+require('dotenv').config();
 
 const client = new Discord.Client();
+const servers = {}
 
 const prefix = "!"
 
@@ -19,19 +21,57 @@ client.on('message', text => {
   if(!text.content.startsWith(prefix) || text.author.bot) return;
 
   const argument = text.content.slice(prefix.length).split(" ")
-  const command = argument.shift().toLowerCase();
-  // console.log(argument)
+  const command = argument[0]
 
+  //Start Of Command Conditional Statements (SOCCS)
   if(command === "dang") {
-    text.channel.send("<:sadge:759279448074879027>")
+    // console.log(text.author.username);
+    if(text.author.username != "Kuru"){
+      text.channel.send("<:sadge:759279448074879027>")
+    } else {
+      text.channel.send("Made you Look")
+    }
   }else if(command === "joinvoice") {
+    console.log(argument)
     const voiceChannel = text.member.voice.channel;
+    function testplay(connection, text) {
+      let server = servers[text.guild.id];
+      server.dispatcher = connection.play(ytdl(server.queue[0], {filter: "audioonly"}));
+      server.queue.shift();
+      server.dispatcher.on("end", function() {
+        if(server.queue[0]){
+          testplay(connection, text);
+        }else{
+          connection.disconnect();
+        }
+      })
+    }
 
-    console.log(text.member.voice.channel)
     if(!voiceChannel) {
       return text.reply("Please join a channel to use that command.")
     }
-    voiceChannel.join()
+
+    if(!argument[1]) {
+      return text.reply("Please provide a valid youtube link")
+    }
+
+    if(!servers[text.guild.id]) servers[text.guild.id] = {
+      queue: []
+    }
+
+    const server = servers[text.guild.id];
+
+    server.queue.push(argument[1])
+
+    if(!text.guild.voiceConnection) text.member.voice.channel.join().then(connection => {
+      testplay(connection, text)
+    })
+    // voiceChannel.join().then(connection => {
+    //   const dispatcher = connection.play('https://www.youtube.com/watch?v=QqRLVFRe9AU')
+    //   dispatcher.on("end", resign => {
+    //     voiceChannel.leave();
+    //   })
+    // })
   }
 })
 
