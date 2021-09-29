d004f135aab1ee7ae81e13e708e83606c64df7ad
Andres Long <andreslong95@gmail.com>
2021-09-28 20:52:05 -0500

    join vc command added

--- index.js
+++ index.js
@@ -15,5 +15,25 @@ client.on('message', text => {
   console.log(text.content)
   console.log("Sent By:", text.author.username)
   console.log("In channel:", text.channel.name)
+
+  if(!text.content.startsWith(prefix) || text.author.bot) return;
+
+  const argument = text.content.slice(prefix.length).split(" ")
+  const command = argument.shift().toLowerCase();
+  // console.log(argument)
+
+  if(command === "dang") {
+    text.channel.send("<:sadge:759279448074879027>")
+  }else if(command === "joinvoice") {
+    const voiceChannel = text.member.voice.channel;
+
+    console.log(text.member.voice.channel)
+    if(!voiceChannel) {
+      return text.reply("Please join a channel to use that command.")
+    }
+    voiceChannel.join()
+  }
 })
+
+
 client.login(process.env.TOKEN);
